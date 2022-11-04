60e7d309f7ed9ae256fa275da9622a685ca5ca6c
Naomi Maindret <naomi.maindret@hotmail.fr>
2022-11-04 19:13:13 +0100

    add pause for video

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let btnEl = document.querySelector(".btn");
 let closeIconEl = document.querySelector(".close-icon");
 let trailerContainerEl = document.querySelector(".trailer-container");
+let videoEl = document.querySelector("video");
 
 btnEl.addEventListener("click", ()=>{
     trailerContainerEl.classList.remove("active");
@@ -8,4 +9,6 @@ btnEl.addEventListener("click", ()=>{
 
 closeIconEl.addEventListener("click", ()=>{
     trailerContainerEl.classList.add("active");
+    videoEl.pause();
+    videoEl.currentTime = 0;
 });
